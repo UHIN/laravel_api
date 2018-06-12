2cd96edc7e5ef5c8ef6b5806049ec067ab3a522f
kris <kclayton@uhin.org>
2018-06-12 09:50:12 -0600

    Updated handler to accept messages from laravel abort() helper.

--- src/Helpers/Handler.php
+++ src/Helpers/Handler.php
@@ -53,61 +53,64 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        if ($request->wantsJson()) {
-            $errors = [];
-            $meta = [];
-            $http_status = null;
+
+        $errors = [];
+        $meta = [];
+        $http_status = null;
 
 
-            if ($exception instanceof ValidationException && $exception->errors()) {
-                foreach ($exception->errors() as $key => $field) {
-                    foreach ($field as $error) {
-                        $error = preg_replace_callback('/data.([A-z\_\.]+)/', function ($matches) {
-                            return str_replace('.', ' ', $matches[1]);
-                        }, $error);
-                        $errors[] = [
-                            'title' => 'Invalid Attribute',
-                            'detail' => $error,
-                            'status' => $exception->status,
-                            'source' => ['pointer' => '/' . str_replace('.', '/', $key) ]
-                        ];
-                    }
+        if ($exception instanceof ValidationException && $exception->errors()) {
+            foreach ($exception->errors() as $key => $field) {
+                foreach ($field as $error) {
+                    $error = preg_replace_callback('/data.([A-z\_\.]+)/', function ($matches) {
+                        return str_replace('.', ' ', $matches[1]);
+                    }, $error);
+                    $errors[] = [
+                        'title' => 'Invalid Attribute',
+                        'detail' => $error,
+                        'status' => $exception->status,
+                        'source' => ['pointer' => '/' . str_replace('.', '/', $key) ]
+                    ];
                 }
-            } else if ($exception instanceof ModelNotFoundException) {
-                $errors[] = [
-                    'title' => "Resource Not Found",
-                    'status' => (string) Response::HTTP_NOT_FOUND
-                ];
-            } else if ($exception instanceof HttpException) {
-                $errors[] = [
-                    'title' => Response::$statusTexts[$exception->getStatusCode()],
-                    'status' => (string) $exception->getStatusCode()
-                ];
-            } else {
-                $errors[] = [
-                    'title' => "An internal server error has occurred. If you continue to experience issues, please contact customer service. This error has been logged.",
-                    'status' => (string) Response::HTTP_INTERNAL_SERVER_ERROR
-                ];
             }
-
-            if (count($errors) == 0) {
-                $errors[] = [
-                    'title' => "An internal server error has occurred. If you continue to experience issues, please contact customer service. This error has been logged.",
-                    'status' => (string) Response::HTTP_INTERNAL_SERVER_ERROR
-                ];
-            }
-            if ($http_status == null) {
-                $http_status = isset($errors[0]['status'])?$errors[0]['status']:500;
-            }
-
-            $json = ['errors' => $errors];
-
-            if (!empty($meta)) {
-                $json['meta'] = $meta;
-            }
-
-            return response()->json($json, $http_status);
+        } else if ($exception instanceof ModelNotFoundException) {
+            $errors[] = [
+                'title' => "Resource Not Found",
+                'status' => (string) Response::HTTP_NOT_FOUND,
+                'detail' => $exception->getMessage()
+            ];
+        } else if ($exception instanceof HttpException) {
+            $errors[] = [
+                'title' => Response::$statusTexts[$exception->getStatusCode()],
+                'status' => (string) $exception->getStatusCode(),
+                'detail' => $exception->getMessage()
+            ];
+        } else {
+            $errors[] = [
+                'title' => "An internal server error has occurred. If you continue to experience issues, please contact customer service. This error has been logged.",
+                'status' => (string) Response::HTTP_INTERNAL_SERVER_ERROR,
+                'detail' => $exception->getMessage()
+            ];
         }
-        return parent::render($request, $exception);
+
+        if (count($errors) == 0) {
+            $errors[] = [
+                'title' => "An internal server error has occurred. If you continue to experience issues, please contact customer service. This error has been logged.",
+                'status' => (string) Response::HTTP_INTERNAL_SERVER_ERROR,
+                'detail' => $exception->getMessage()
+            ];
+        }
+        if ($http_status == null) {
+            $http_status = isset($errors[0]['status'])?$errors[0]['status']:500;
+        }
+
+        $json = ['errors' => $errors];
+
+        if (!empty($meta)) {
+            $json['meta'] = $meta;
+        }
+
+        return response()->json($json, $http_status);
+
     }
 }
